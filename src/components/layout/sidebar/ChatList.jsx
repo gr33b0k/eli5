dcdaa766f7e65edf548b383f0398af9eca97ee72
Ilya <sovetniy@bk.ru>
2026-04-07 15:25:40 +0300

    refactor: change sidebar layout and styles

--- src/components/layout/sidebar/ChatList.jsx
+++ src/components/layout/sidebar/ChatList.jsx
@@ -4,7 +4,10 @@ function ChatList({ chats }) {
       {chats.map((chat) => (
         <li
           key={chat.id}
-          className={`cursor-pointer rounded-sm px-3 py-2 text-base transition-colors duration-300 ease-in-out ${chat.isActive ? "border border-indigo-700/50 bg-indigo-300/30 dark:border-indigo-300/20 dark:bg-indigo-300/20" : "hover:bg-indigo-300 dark:hover:bg-indigo-400/20"} `}
+          className={
+            "hover:bg-surface/70 cursor-pointer rounded-sm px-3 py-2 text-base transition-colors duration-300 ease-in-out" +
+            (chat.isActive ? " glass shadow-even" : "")
+          }
         >
           {chat.title}
         </li>
--- src/components/layout/sidebar/Sidebar.jsx
+++ src/components/layout/sidebar/Sidebar.jsx
@@ -1,28 +1,28 @@
-import { BookIcon } from "@phosphor-icons/react";
-
 import ChatList from "./ChatList";
 
 function Sidebar() {
   return (
-    <aside className="w-70 border-r border-indigo-700/20 bg-indigo-300/20 p-4 dark:border-indigo-700/50 dark:bg-slate-100/5">
-      <div className="border-b-gray mb-4 flex items-center gap-2">
-        <div className="rounded-md bg-indigo-900 p-1.5">
-          <BookIcon className="h-6 w-6 text-indigo-100" />
+    <aside className="glass flex w-80 flex-col gap-6 px-6 py-8">
+      <h1 className="text-text flex items-center gap-2">
+        <span className="block text-3xl/4 font-bold">ELI5</span>
+        <div className="text-xs">
+          <span className="block">Explain it like</span>
+          <span className="block">i'm five</span>
         </div>
-        <h1 className="text-2xl font-bold text-indigo-900 dark:text-indigo-100">
-          ELI5
-        </h1>
+      </h1>
+      <button className="bg-accent rounded-md px-4 py-2 text-white">
+        + New Chat
+      </button>
+      <div>
+        <h2 className="text-text-muted mb-2 text-sm font-semibold">Chats</h2>
+        <ChatList
+          chats={[
+            { id: 1, title: "Chat-1", isActive: false },
+            { id: 2, title: "Chat-2", isActive: true },
+            { id: 3, title: "Chat-3", isActive: false },
+          ]}
+        />
       </div>
-      <h2 className="mb-2 text-lg font-semibold text-indigo-900 dark:text-indigo-100">
-        Chats
-      </h2>
-      <ChatList
-        chats={[
-          { id: 1, title: "Chat-1", isActive: false },
-          { id: 2, title: "Chat-2", isActive: true },
-          { id: 3, title: "Chat-3", isActive: false },
-        ]}
-      />
     </aside>
   );
 }
