824fc068a7aa32f4d289e6c87b1f1071f04bea24
Ilya <sovetniy@bk.ru>
2026-04-08 19:21:52 +0300

    refactor: remove text-base class from ChatList items

--- src/components/layout/sidebar/ChatList.jsx
+++ src/components/layout/sidebar/ChatList.jsx
@@ -5,7 +5,7 @@ function ChatList({ chats }) {
         <li
           key={chat.id}
           className={
-            "hover:bg-surface/70 text-text cursor-pointer rounded-3xl px-3 py-2 text-base transition-colors duration-300 ease-in-out" +
+            "hover:bg-surface/70 text-text cursor-pointer rounded-3xl px-3 py-2 transition-colors duration-300 ease-in-out" +
             (chat.isActive ? " glass" : "")
           }
         >
