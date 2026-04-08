e038b955080151d73338d8839755d8177297a27d
Ilya <sovetniy@bk.ru>
2026-04-08 16:01:30 +0300

    refactor: simplify main layout structure

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,13 +2,9 @@ import Sidebar from "./sidebar/Sidebar";
 
 function MainLayout({ children }) {
   return (
-    <div className="bg-gradient relative min-h-screen overflow-hidden">
-      <div className="flex h-screen">
-        <Sidebar />
-        <main className="flex flex-1 flex-col gap-4 px-34 py-20">
-          {children}
-        </main>
-      </div>
+    <div className="bg-gradient relative flex min-h-screen overflow-hidden p-8">
+      <Sidebar />
+      <main className="flex flex-1 flex-col gap-4 pl-8">{children}</main>
     </div>
   );
 }
