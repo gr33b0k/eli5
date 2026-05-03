2306f5b7ad90cc455cdd5eec8b07b0558aedba72
Ilya <sovetniy@bk.ru>
2026-05-03 18:41:27 +0300

    chore: add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 3000;
 
 app.post("/api/explain", async (req, res) => {
