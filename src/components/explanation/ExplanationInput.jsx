397e77218041c64e8161b88fc1e6291526bad3c0
Ilya <sovetniy@bk.ru>
2026-05-03 11:17:53 +0300

    feat: handle input submission

--- src/components/explanation/ExplanationInput.jsx
+++ src/components/explanation/ExplanationInput.jsx
@@ -1,8 +1,15 @@
-function ExplanationInput() {
+function ExplanationInput({ onSubmit }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && onSubmit) {
+      onSubmit(e.target.value);
+    }
+  };
+
   return (
     <input
+      onKeyDown={handleKeyDown}
       className="glass text-text caret-text active:shadow-accent focus:shadow-primary focus:shadow-even rounded-4xl px-5 py-4 outline-0 transition-shadow duration-300 ease-in-out"
-      placeholder="What do you want to understand"
+      placeholder="What topic do you want to break down?"
     />
   );
 }
