4a82117c72aff93386eac8bfc4ed3d4892c00648
Ilya <sovetniy@bk.ru>
2026-05-19 18:11:33 +0300

    feat: add animations to header

--- client/src/widgets/header/ui/Header.jsx
+++ client/src/widgets/header/ui/Header.jsx
@@ -1,12 +1,29 @@
+import { motion } from "motion/react";
+
 import { Logo } from "@/shared/ui";
 
 function Header() {
   return (
-    <header className="flex w-full justify-between rounded-4xl px-10 py-5">
+    <motion.header
+      initial={{
+        opacity: 0,
+        y: -30,
+        filter: "blur(10px)",
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        filter: "blur(0px)",
+      }}
+      transition={{
+        duration: 0.8,
+      }}
+      className="flex w-full justify-between rounded-4xl px-10 py-5"
+    >
       <Logo />
 
       <button className="btn-primary">Get started</button>
-    </header>
+    </motion.header>
   );
 }
 
