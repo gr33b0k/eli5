e980540f2031d9f6d96561cc273726fd03bbf817
Ilya <sovetniy@bk.ru>
2026-06-25 18:45:11 +0300

    feat: add optional animation to Header component

--- client/src/widgets/header/ui/Header.jsx
+++ client/src/widgets/header/ui/Header.jsx
@@ -2,23 +2,25 @@ import { motion } from "motion/react";
 
 import { Logo } from "@/shared/ui";
 
-function Header({ children }) {
+function Header({ animate = true, children }) {
   return (
     <motion.header
-      initial={{
-        opacity: 0,
-        y: -30,
-        filter: "blur(10px)",
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-        filter: "blur(0px)",
-      }}
-      transition={{
-        duration: 0.5,
-      }}
-      className="flex w-full justify-between rounded-4xl px-10 py-5"
+      {...(animate && {
+        initial: {
+          opacity: 0,
+          y: -30,
+          filter: "blur(10px)",
+        },
+        animate: {
+          opacity: 1,
+          y: 0,
+          filter: "blur(0px)",
+        },
+        transition: {
+          duration: 0.5,
+        },
+      })}
+      className="z-10 flex min-h-20 w-full justify-between rounded-4xl px-10 py-5"
     >
       <Logo />
 
