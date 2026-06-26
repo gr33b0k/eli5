45e4afafe1613021eb168696006c7b4912b6a67c
Ilya <sovetniy@bk.ru>
2026-06-26 15:50:11 +0300

    style: replace 'remember me' checkbox with toggle switch

--- client/src/features/auth/ui/AuthForm.jsx
+++ client/src/features/auth/ui/AuthForm.jsx
@@ -45,16 +45,27 @@ function AuthForm({ mode = "login" }) {
         )}
 
         {isLogin && (
-          <div className="flex items-center justify-between">
-            <label className="text-text flex items-center gap-2 text-sm">
-              <input type="checkbox" className="border-border outline-border" />
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-2">
+              <button
+                type="button"
+                className={`text-text shadow-primary/50 inset-shadow-surface/30 ring-accent/50 flex h-5 w-9 cursor-pointer rounded-full p-0.5 ring ${remember ? "bg-accent" : "bg-accent/20 "}`}
+                style={{
+                  justifyContent: "flex-" + (remember ? "end" : "start"),
+                }}
+                onClick={() => setRemember(!remember)}
+              >
+                <motion.div
+                  className="bg-surface h-4 w-4 rounded-full"
+                  layout
+                  transition={{
+                    duration: 0.2,
+                  }}
+                />
+              </button>
               Remember me
-            </label>
-
-            <button
-              type="button"
-              className="text-accent text-sm hover:underline"
-            >
+            </div>
+            <button type="button" className="text-accent hover:underline">
               Forgot password?
             </button>
           </div>
