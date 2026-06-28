e9b9ec0d162af51308680e680a6c4094265d5746
Ilya <sovetniy@bk.ru>
2026-06-28 22:05:25 +0300

    feat: add user global store

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,11 @@
 import { RouterProvider } from "react-router";
 import { router } from "./providers/router/router";
+import { useUserStore } from "@/entities/user";
 
 function App() {
+  const user = useUserStore((state) => state.user);
+  const setUser = useUserStore((state) => state.setUser);
+
   return <RouterProvider router={router} />;
 }
 
