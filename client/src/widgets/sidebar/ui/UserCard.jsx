60ecb0386bd401c503c90ac0581aafac91163bd1
Ilya <sovetniy@bk.ru>
2026-07-03 09:42:08 +0300

    style: change user avatar's text size

--- client/src/widgets/sidebar/ui/UserCard.jsx
+++ client/src/widgets/sidebar/ui/UserCard.jsx
@@ -2,11 +2,11 @@ import { SignOutIcon } from "@phosphor-icons/react";
 
 function UserCard({ user, onLogout }) {
   return (
-    <div className="glass-40 flex items-center justify-between rounded-4xl px-7 py-3">
+    <div className="glass-40 flex min-h-19.5 items-center justify-between rounded-4xl px-7 py-4">
       <div className="flex items-center gap-3">
-        <div className="bg-accent text-surface flex h-13.5 w-13.5 items-center justify-center rounded-full text-2xl">
+        <span className="bg-accent text-surface font-miranda flex h-10 w-10 items-center justify-center rounded-full text-xl">
           {user.username[0]}
-        </div>
+        </span>
         <p className="text-text">{user.username}</p>
       </div>
       <button
