3f301c4e2a1ead979cf7041a708fe8496dd011fa
Ilya <sovetniy@bk.ru>
2026-07-04 14:58:05 +0300

    feat: load user chats on app startup

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,16 +2,34 @@ import { useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { router } from "./providers/router/router";
 import { useUserStore, getMe } from "@/entities/user";
+import { useChatStore, getChats } from "@/entities/chat";
 
 function App() {
   const user = useUserStore((state) => state.user);
   const setUser = useUserStore((state) => state.setUser);
+  const setChats = useChatStore((state) => state.setChats);
 
   useEffect(() => {
-    getMe()
-      .then((user) => user && setUser(user))
-      .catch(console.error);
-  }, [setUser]);
+    async function init() {
+      try {
+        const user = await getMe();
+
+        if (user) {
+          setUser(user);
+
+          const chats = await getChats(user.id);
+
+          if (chats) {
+            setChats(chats);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    init();
+  }, [setUser, setChats]);
 
   return <RouterProvider router={router} />;
 }
