3d102cd6fc1f94395d0094469840d6ebd2b123ed
Ilya <sovetniy@bk.ru>
2026-07-04 14:59:42 +0300

    feat: display user chats in sidebar

--- client/src/widgets/sidebar/ui/ChatList.jsx
+++ client/src/widgets/sidebar/ui/ChatList.jsx
@@ -1,4 +1,4 @@
-function ChatList({ chats }) {
+function ChatList({ chats, activeChat, onClick }) {
   return (
     <ul className="custom-scrollbar flex flex-col gap-1 overflow-y-auto px-4">
       {chats.map((chat, index) => (
@@ -6,8 +6,9 @@ function ChatList({ chats }) {
           key={chat.id}
           className={
             "text-text cursor-pointer rounded-4xl px-3 py-2 transition-colors duration-300 ease-in-out " +
-            (chat.isActive ? "glass-50" : "hover:bg-surface/60")
+            (chat.id === activeChat ? "glass-50" : "hover:bg-surface/60")
           }
+          onClick={() => onClick(chat.id)}
         >
           {chat.title}
         </li>
--- client/src/widgets/sidebar/ui/Sidebar.jsx
+++ client/src/widgets/sidebar/ui/Sidebar.jsx
@@ -1,14 +1,26 @@
 import { useUserStore } from "../../../entities/user";
 import ChatList from "./ChatList";
 import UserCard from "./UserCard";
+
 import { logout } from "@/entities/user";
+import { useChatStore, createChat, getChats } from "@/entities/chat";
+
 import { Logo } from "@/shared/ui";
 
 function Sidebar() {
   const user = useUserStore((state) => state.user);
   const clearUser = useUserStore((state) => state.clearUser);
 
-  console.log(user);
+  const chats = useChatStore((state) => state.chats);
+  const activeChat = useChatStore((state) => state.activeChatId);
+  const addChat = useChatStore((state) => state.addChat);
+  const setChats = useChatStore((state) => state.setChats);
+  const setActiveChat = useChatStore((state) => state.setActiveChat);
+
+  async function handleCreateChat() {
+    const chat = await createChat();
+    addChat(chat);
+  }
 
   async function handleLogout() {
     await logout();
@@ -22,34 +34,13 @@ function Sidebar() {
       </div>
 
       <div className="glass-40 flex flex-1 flex-col gap-3 overflow-hidden rounded-4xl py-4 pb-7">
-        <button className="btn-primary mx-4">+ New Chat</button>
+        <button className="btn-primary mx-4" onClick={handleCreateChat}>
+          + New Chat
+        </button>
         <ChatList
-          chats={[
-            { id: 1, title: "Chat-1", isActive: false },
-            { id: 2, title: "Chat-2", isActive: true },
-            { id: 3, title: "Chat-3", isActive: false },
-            { id: 4, title: "Chat-3", isActive: false },
-            { id: 5, title: "Chat-3", isActive: false },
-            { id: 6, title: "Chat-3", isActive: false },
-            { id: 7, title: "Chat-3", isActive: false },
-            { id: 8, title: "Chat-3", isActive: false },
-            { id: 9, title: "Chat-3", isActive: false },
-            { id: 10, title: "Chat-3", isActive: false },
-            { id: 33, title: "Chat-3", isActive: false },
-            { id: 34, title: "Chat-3", isActive: false },
-            { id: 35, title: "Chat-3", isActive: false },
-            { id: 36, title: "Chat-3", isActive: false },
-            { id: 37, title: "Chat-3", isActive: false },
-            { id: 38, title: "Chat-3", isActive: false },
-            { id: 31, title: "Chat-3", isActive: false },
-            { id: 32, title: "Chat-3", isActive: false },
-            { id: 53, title: "Chat-3", isActive: false },
-            { id: 423, title: "Chat-3", isActive: false },
-            { id: 3242, title: "Chat-3", isActive: false },
-            { id: 423423, title: "Chat-3", isActive: false },
-            { id: 442543423, title: "Chat-3", isActive: false },
-            { id: 4351535, title: "Chat-3", isActive: false },
-          ]}
+          chats={chats}
+          activeChat={activeChat}
+          onClick={setActiveChat}
         />
       </div>
       {user && <UserCard user={user} onLogout={handleLogout} />}
